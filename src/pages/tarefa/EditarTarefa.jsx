14cf78f9d14c22376f430601054cfa02da1f9aa7
Victor Paiva <paiva_victor@live.com>
2024-06-02 18:45:49 -0300

    Novas funcionalidades

--- src/pages/tarefa/EditarTarefa.jsx
+++ src/pages/tarefa/EditarTarefa.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { FormControl, InputLabel, Input, FormHelperText } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
@@ -9,9 +9,7 @@ import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 
-//Declaração do componente EditarTarefa, recebendo como props, do Componente ListarTarefa, os states handCloseEditar,
-// idTarefaSelecionada, tarefas, tarefa e setTarefas
-const EditarTarefa = ({handleCloseEditar, idTarefaSelecionada, tarefas, tarefa, setTarefas}) =>{
+const CriarTarefa = ({ handleClose, tarefas, setTarefas }) => {
   const [idTarefa, setIdTarefa] = useState();
   const [tituloTarefa, setTituloTarefa] = useState('');
   const [descricaoTarefa, setDescricaoTarefa] = useState('');
@@ -20,18 +18,10 @@ const EditarTarefa = ({handleCloseEditar, idTarefaSelecionada, tarefas, tarefa, 
   const [recursoTarefa, setRecursoTarefa] = useState('');
   const [statusTarefa, setStatusTarefa] = useState('');
 
-  //Abaixo setamos os valores dos states (que popularão o formulário mais abaixo) com os valores do state Tarefa,
-  //  recebido como props do componente ListarTarefa.
   useEffect(() => {
-    //console.log('Tarefa selecionada: ' + JSON.stringify(tarefa));
-    setIdTarefa(idTarefaSelecionada);
-    setTituloTarefa(tarefa.tituloTarefa);
-    setDescricaoTarefa(tarefa.descricaoTarefa);
-    setInicioTarefa(tarefa.inicioTarefa);
-    setFimTarefa(tarefa.fimTarefa);
-    setRecursoTarefa(tarefa.recursoTarefa);
-    setStatusTarefa(tarefa.statusTarefa);
-  },[]);
+    let proximoId = Math.max(...tarefas.map(tarefa => tarefa.idTarefa)) + 1;
+    setIdTarefa(proximoId);
+  }, []);
 
   const handleRecurso = (event) => {
     setRecursoTarefa(event.target.value);
@@ -41,39 +31,31 @@ const EditarTarefa = ({handleCloseEditar, idTarefaSelecionada, tarefas, tarefa, 
     setStatusTarefa(event.target.value);
   };
 
-  const handleEditar = () => {
-    //console.log(`id: ${idTarefa} \n titulo: ${tituloTarefa} \n descrição: ${descricaoTarefa} \n inicio: ${inicioTarefa} \n fim: ${fimTarefa} \n recurso: ${recursoTarefa} \n status: ${statusTarefa}`);
-    //console.log('idTarefaSelecionada: ' + idTarefaSelecionada);
-    setTarefas(current =>
-      current.map(obj => {
-        if (obj.idTarefa === idTarefaSelecionada) {
-          console.log('obj: ' + JSON.stringify(obj));          
-          return {...obj, 
-              idTarefa:idTarefaSelecionada,
-              tituloTarefa:tituloTarefa,
-              descricaoTarefa:descricaoTarefa,
-              inicioTarefa:inicioTarefa,
-              fimTarefa:fimTarefa,
-              recursoTarefa:recursoTarefa,
-              statusTarefa:statusTarefa
-          };
-        }
+  const handleSalvar = () => {
+    console.log(`id: ${idTarefa} \n titulo: ${tituloTarefa} \n descrição: ${descricaoTarefa} \n inicio: ${inicioTarefa} \n fim: ${fimTarefa} \n recurso: ${recursoTarefa} \n status: ${statusTarefa}`);
 
-        return obj;
-      }),
+    setTarefas(
+      [...tarefas,
+      {
+        idTarefa,
+        tituloTarefa,
+        descricaoTarefa,
+        inicioTarefa,
+        fimTarefa,
+        recursoTarefa,
+        statusTarefa
+      }]
     );
-
-    //console.log(`Tarefas Editadas: ` + JSON.stringify(tarefas));
-    handleCloseEditar();
+    handleClose();
   };
 
-  return(
+  return (
     <Grid container spacing={2}>
       <Card sx={style}>
         <CardHeader
           title="Tarefas"
-          subheader="Edição de Tarefas"
-        /> 
+          subheader="Cadastro de Tarefas"
+        />
         <CardContent sx={{
           width: '95%',
           maxWidth: '100%',
@@ -84,38 +66,38 @@ const EditarTarefa = ({handleCloseEditar, idTarefaSelecionada, tarefas, tarefa, 
               <FormHelperText id="tarefa_titulo_helper_text">Título da Tarefa.</FormHelperText>
             </FormControl>
           </Grid>
-          <Grid item xs={12}>  
+          <Grid item xs={12}>
             <FormControl fullWidth>
               <Input id="tarefa_descricao" aria-describedby="tarefa_descricao_helper_text" value={descricaoTarefa} onChange={e => { setDescricaoTarefa(e.target.value) }} />
               <FormHelperText id="tarefa_descricao_helper_text">Descrição da Tarefa.</FormHelperText>
             </FormControl>
           </Grid>
           <Grid container spacing={2} mt={1}>
-            <Grid item xs={3}>  
+            <Grid item xs={3}>
               <FormControl>
                 <Input id="tarefa_inicio" type="date" aria-describedby="tarefa_inicio_helper_text" value={inicioTarefa} onChange={e => { setInicioTarefa(e.target.value) }}
                   sx={{
-                    color:'rgba(0, 0, 0, 0.6)',
+                    color: 'rgba(0, 0, 0, 0.6)',
                     fontWeight: 400,
-                    paddingLeft:'13px'
-                  }} 
+                    paddingLeft: '13px'
+                  }}
                 />
                 <FormHelperText id="tarefa_inicio_helper_text">Início da Tarefa.</FormHelperText>
               </FormControl>
-            </Grid>  
-            <Grid item xs={3}>  
+            </Grid>
+            <Grid item xs={3}>
               <FormControl>
                 <Input id="tarefa_fim" type="date" aria-describedby="tarefa_fim_helper_text" value={fimTarefa} onChange={e => { setFimTarefa(e.target.value) }}
                   sx={{
-                    color:'rgba(0, 0, 0, 0.6)',
+                    color: 'rgba(0, 0, 0, 0.6)',
                     fontWeight: 400,
-                    paddingLeft:'13px'
-                  }} 
+                    paddingLeft: '13px'
+                  }}
                 />
                 <FormHelperText id="tarefa_fim_helper_text">Fim da Tarefa.</FormHelperText>
               </FormControl>
             </Grid>
-            <Grid item xs={3}>  
+            <Grid item xs={3}>
               <FormControl fullWidth>
                 <InputLabel htmlFor="tarefa_recurso">Recurso</InputLabel>
                 <Select
@@ -125,59 +107,8 @@ const EditarTarefa = ({handleCloseEditar, idTarefaSelecionada, tarefas, tarefa, 
                   onChange={handleRecurso}
                   size="small"
                   sx={{
-                    color:'rgba(0, 0, 0, 0.6)',
+                    color: 'rgba(0, 0, 0, 0.6)',
                     fontWeight: 400,
-                  }} 
+                  }}
                 >
-                  <MenuItem value={'Recurso 1'}>Recurso 1</MenuItem>
-                  <MenuItem value={'Recurso 2'}>Recurso 2</MenuItem>
-                  <MenuItem value={'Recurso 3'}>Recurso 3</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={3}>  
-              <FormControl fullWidth>
-                <InputLabel htmlFor="tarefa_recurso">Status</InputLabel>
-                <Select
-                  id="tarefa_status"
-                  value={statusTarefa}
-                  label="Status"
-                  onChange={handleStatus}
-                  size="small"
-                  sx={{
-                    color:'rgba(0, 0, 0, 0.6)',
-                    fontWeight: 400,
-                  }} 
-                >
-                  <MenuItem value={'Aguardando'}>Aguardando</MenuItem>
-                  <MenuItem value={'Em Andamento'}>Em Andamento</MenuItem>
-                  <MenuItem value={'Concluída'}>Concluída</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid container spacing={2} pl={2} mt={2}>
-              <Grid item xs={1}>
-                <Button size="small" variant="contained" onClick={handleEditar}>Salvar</Button>
-              </Grid>  
-              <Grid item xs={1}>  
-                <Button size="small" variant="outlined" onClick={handleCloseEditar}>Cancelar</Button>  
-              </Grid>
-            </Grid>  
-          </Grid>
-        </CardContent>
-      </Card>
-    </Grid>
-  );
-}
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '60%',
-  bgcolor: 'background.paper',
-  p: 4,
-};
-
-export default EditarTarefa;
+                 
